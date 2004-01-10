003c46d6d720941b0b0e8302593840ea406cf84e
Ingo Ruhnke <grumbel@gmail.com>
2004-01-10 20:29:13 +0000

    - more controller configs

---
+++ data/controller/gamepad.scm
@@ -0,0 +1,11 @@
+(feuerkraft-controller
+ (up    (axis-button (joystick-axis 0 1) #t))
+ (down  (axis-button (joystick-axis 0 1) #f))
+ (left  (axis-button (joystick-axis 0 0) #t))
+ (right (axis-button (joystick-axis 0 0) #f))
+
+ (fire  (joystick-button 0 1))
+ (jump  (joystick-button 0 0))
+ )
+
+;; EOF ;;
---
+++ data/controller/gamepad2.scm
@@ -0,0 +1,11 @@
+(feuerkraft-controller
+ (up    (axis-button (joystick-axis 1 1) #t))
+ (down  (axis-button (joystick-axis 1 1) #f))
+ (left  (axis-button (joystick-axis 1 0) #t))
+ (right (axis-button (joystick-axis 1 0) #f))
+
+ (fire  (joystick-button 1 1))
+ (jump  (joystick-button 1 0))
+ )
+
+;; EOF ;;

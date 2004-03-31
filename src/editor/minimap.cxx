daff64daf71bb35471e76fcca57634b0c574d266
Ingo Ruhnke <grumbel@gmail.com>
2004-03-31 18:06:30 +0000

    - made minimap a little bit more crash save

--- src/editor/minimap.cxx
+++ src/editor/minimap.cxx
@@ -66,45 +66,47 @@ Minimap::draw()
     }
 
   // FIXME: This doesn't work all that well
-  EditorTileMap* tilemap = dynamic_cast<EditorTileMap*>
-    (EditorMapComponent::current()->get_map()->get_layer(0));
+  EditorTileMap* tilemap = EditorTileMap::current();
 
-  int map_width  = tilemap->get_width()  * TILE_SIZE;
-  int map_height = tilemap->get_height() * TILE_SIZE;
-
-  CL_Size small_tile(TILE_SIZE * get_width() / map_width + 1,
-                     TILE_SIZE * get_height() / map_height + 1);
-
-  Field<int>* field = tilemap->get_map();
-
-  if (0)
+  if (tilemap)
     {
-      for(int y = 0; y < field->get_height(); ++y)
-        for(int x = 0; x < field->get_width(); ++x)
-          {
-            Tile* tile = TileFactory::current()->create(field->at(x, y));
-            if (tile)
-              CL_Display::fill_rect(CL_Rect(CL_Point((x * TILE_SIZE) * get_width() / map_width,
-                                                     (y * TILE_SIZE) * get_height() / map_height),
-                                            small_tile),
-                                    tile->get_color());
-            CL_Display::flush();
-          }
+      int map_width  = tilemap->get_width()  * TILE_SIZE;
+      int map_height = tilemap->get_height() * TILE_SIZE;
+
+      CL_Size small_tile(TILE_SIZE * get_width() / map_width + 1,
+                         TILE_SIZE * get_height() / map_height + 1);
+
+      Field<int>* field = tilemap->get_map();
+
+      if (0)
+        {
+          for(int y = 0; y < field->get_height(); ++y)
+            for(int x = 0; x < field->get_width(); ++x)
+              {
+                Tile* tile = TileFactory::current()->create(field->at(x, y));
+                if (tile)
+                  CL_Display::fill_rect(CL_Rect(CL_Point((x * TILE_SIZE) * get_width() / map_width,
+                                                         (y * TILE_SIZE) * get_height() / map_height),
+                                                small_tile),
+                                        tile->get_color());
+                CL_Display::flush();
+              }
+        }
+
+      minimap_surface.draw(CL_Rect(CL_Point(0, 0),
+                                   CL_Size(get_width(), get_height())));
+
+      // Draw cursor
+      CL_Rect rect = parent->get_clip_rect();
+      CL_Rect screen_rect(CL_Point(rect.left  * get_width()  / map_width,
+                                   rect.top   * get_height() / map_height),
+                          CL_Size(rect.get_width() * get_width() /map_width,
+                                  rect.get_height()* get_height()/map_height));
+      CL_Display::fill_rect(screen_rect,
+                            CL_Color(255, 255, 0, 50));
+      CL_Display::draw_rect(screen_rect,
+                            CL_Color(0, 0, 0));
     }
-
-  minimap_surface.draw(CL_Rect(CL_Point(0, 0),
-                               CL_Size(get_width(), get_height())));
-
-  // Draw cursor
-  CL_Rect rect = parent->get_clip_rect();
-  CL_Rect screen_rect(CL_Point(rect.left  * get_width()  / map_width,
-                               rect.top   * get_height() / map_height),
-                      CL_Size(rect.get_width() * get_width() /map_width,
-                              rect.get_height()* get_height()/map_height));
-  CL_Display::fill_rect(screen_rect,
-                        CL_Color(255, 255, 0, 50));
-  CL_Display::draw_rect(screen_rect,
-                        CL_Color(0, 0, 0));
 }
 
 void
@@ -113,36 +115,39 @@ Minimap::update_minimap_surface()
   // FIXME: This doesn't work all that well
   EditorTileMap* tilemap = EditorTileMap::current();
 
-  Field<int>* field = tilemap->get_map();
+  if (tilemap)
+    {
+      Field<int>* field = tilemap->get_map();
 
-  CL_PixelBuffer buffer(tilemap->get_width(), tilemap->get_height(), 
-                        tilemap->get_width()*4, CL_PixelFormat::rgba8888);
+      CL_PixelBuffer buffer(tilemap->get_width(), tilemap->get_height(), 
+                            tilemap->get_width()*4, CL_PixelFormat::rgba8888);
   
-  int map_width  = tilemap->get_width();
-  int map_height = tilemap->get_height();
+      int map_width  = tilemap->get_width();
+      int map_height = tilemap->get_height();
 
-  unsigned char* buf = static_cast<unsigned char*>(buffer.get_data());
-  for(int y = 0; y < map_height; ++y)
-    for(int x = 0; x < map_width; ++x)
-      {
-        Tile* tile = TileFactory::current()->create(field->at(x, y));
-        if (tile)
+      unsigned char* buf = static_cast<unsigned char*>(buffer.get_data());
+      for(int y = 0; y < map_height; ++y)
+        for(int x = 0; x < map_width; ++x)
           {
-            buf[4*(x + y * map_width) + 3] = tile->get_color().get_red();
-            buf[4*(x + y * map_width) + 2] = tile->get_color().get_green();
-            buf[4*(x + y * map_width) + 1] = tile->get_color().get_blue();
-            buf[4*(x + y * map_width) + 0] = tile->get_color().get_alpha();
-          } 
-        else
-          {
-            buf[4*(x + y * map_width) + 0] = 0;
-            buf[4*(x + y * map_width) + 1] = 0;
-            buf[4*(x + y * map_width) + 2] = 0;
-            buf[4*(x + y * map_width) + 3] = 0;
+            Tile* tile = TileFactory::current()->create(field->at(x, y));
+            if (tile)
+              {
+                buf[4*(x + y * map_width) + 3] = tile->get_color().get_red();
+                buf[4*(x + y * map_width) + 2] = tile->get_color().get_green();
+                buf[4*(x + y * map_width) + 1] = tile->get_color().get_blue();
+                buf[4*(x + y * map_width) + 0] = tile->get_color().get_alpha();
+              } 
+            else
+              {
+                buf[4*(x + y * map_width) + 0] = 0;
+                buf[4*(x + y * map_width) + 1] = 0;
+                buf[4*(x + y * map_width) + 2] = 0;
+                buf[4*(x + y * map_width) + 3] = 0;
+              }
           }
-      }
 
-  minimap_surface = CL_Surface(&buffer);
+      minimap_surface = CL_Surface(&buffer);
+    }
 }
 
 void

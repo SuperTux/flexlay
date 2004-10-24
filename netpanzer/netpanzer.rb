94b98ca986044a95b28a1360a603cf96bfc7c6c6
Ingo Ruhnke <grumbel@gmail.com>
2004-10-24 14:43:43 +0000

    - some more fixed to netpanzer module

--- netpanzer/netpanzer.rb
+++ netpanzer/netpanzer.rb
@@ -43,7 +43,7 @@ class Level
       (width, height) = params
       @data = NetPanzerFileStruct.new($tileset, width, height)
 
-    elsif len(params) == 1 then
+    elsif params.length == 1 then
       (@filename,) = params
       @data = NetPanzerFileStruct.new($tileset, @filename)
     end      
@@ -58,31 +58,31 @@ class Level
   end
 
   def save_optfile(filename)
-    outpots = [] # FIXME
+    outposts = [] # FIXME
     
     f = open(filename, "w")
-    f.write("ObjectiveCount: %d\n\n" % len(outposts))
-    for (name, x , y) in outpots:
+    f.write("ObjectiveCount: %d\n\n" % outposts.length)
+    outposts.each {|name, x , y|
         f.write("Name: %s\n" % "Foobar")
       f.write("Location: %d %d\n\n" % x.to_i/32, y.to_i/32)
-    end
+    }
   end
 
   def save_spnfile(filename)
     spawnpoints = []
     f = open(filename, "w")
 
-    f.write("SpawnCount: %d\n\n" % len(spawnpoints))
+    f.write("SpawnCount: %d\n\n" % spawnpoints.length)
     spawnpoints.each {|x, y|
       f.print("Location: %d %d\n" % [x.to_i/32, y.to_i/32])
     }
   end
 
   def save(filename)
-    if filename[-4..-1] == ".npm":
-        data.save(filename)
-      save_optfile(filename[0..-4] + ".opt")
-      save_optfile(filename[0..-4] + ".spn")
+    if filename[-4..-1] == ".npm"
+      data.save(filename)
+      save_optfile(filename[0..-5] + ".opt")
+      save_optfile(filename[0..-5] + ".spn")
     else
       raise "Fileextension not valid, must be .npm!"
     end
@@ -154,60 +154,46 @@ end
 
 startlevel = Level.new(256, 256)
 startlevel.activate($workspace)
-connect(startlevel.editormap.sig_change(), on_map_change)
-
-button_panel = Panel.new(CL_Rect.new(CL_Point.new(0, 23), CL_Size.new(800, 33)), $gui.get_component())
+connect(startlevel.editormap.sig_change(), proc{on_map_change})
 
 def gui_level_save_as()
-  $save_dialog.set_filename(os.path.dirname($save_dialog.get_filename()) + "/")
-  $save_dialog.run(netpanzer_save_level)
+  $save_dialog.set_filename(File::dirname($save_dialog.get_filename()) + "/")
+  $save_dialog.run(method(:netpanzer_save_level))
 end
 
 def gui_level_save()
   if $workspace.get_map().get_metadata().filename:
       $save_dialog.set_filename($workspace.get_map().get_metadata().filename)
   else
-    $save_dialog.set_filename(os.path.dirname($save_dialog.get_filename())  + "/")
+    $save_dialog.set_filename(File::dirname($save_dialog.get_filename())  + "/")
   end
   
-  $save_dialog.run(netpanzer_save_level)
+  $save_dialog.run(method(:netpanzer_save_level))
 end
 
 def gui_level_load()
-  $load_dialog.run(netpanzer_load_level)
+  $load_dialog.run(method(:netpanzer_load_level))
 end
 
-# new_icon         = Icon.new(CL_Rect.new(CL_Point.new(p.inc(0),  2), CL_Size.new(32, 32)),
-#                         make_sprite("../data/images/icons24/stock_new.png"), "Some tooltip", button_panel);
-# load_icon        = Icon(CL_Rect(CL_Point(p.inc(32), 2), CL_Size(32, 32)),
-#                         make_sprite("../data/images/icons24/stock_open.png"), "Some tooltip", button_panel);
-# load_recent_icon = Icon(CL_Rect(CL_Point(p.inc(32), 2), CL_Size(16, 32)),
-#                         make_sprite("../data/images/icons24/downarrow.png"), "Some tooltip", button_panel);
-# save_icon        = Icon(CL_Rect(CL_Point(p.inc(16), 2), CL_Size(32, 32)),
-#                         make_sprite("../data/images/icons24/stock_save.png"), "Some tooltip", button_panel);
-# save_as_icon     = Icon(CL_Rect(CL_Point(p.inc(32), 2), CL_Size(32, 32)),
-#                         make_sprite("../data/images/icons24/stock_save_as.png"), "Some tooltip", button_panel);
+$button_panel = ButtonPanel.new(0, 23, 800, 33, true, $gui.get_component)
 
-# load_icon.set_callback(gui_level_load)
-# load_recent_icon.set_callback(lambda: recent_files_menu.run())
-# save_icon.set_callback(gui_level_save)
-# save_as_icon.set_callback(gui_level_save_as)
+$button_panel.add_icon("../data/images/icons24/stock_new.png",
+                       proc{ gui_level_new() })
+$button_panel.add_icon("../data/images/icons24/stock_open.png", 
+                       proc{ gui_level_load() })
+$button_panel.add_small_icon("../data/images/icons24/downarrow.png", 
+                             proc{ gui_level_load() })
+$button_panel.add_icon("../data/images/icons24/stock_save.png",
+                       proc{ gui_level_save() })
+$button_panel.add_icon("../data/images/icons24/stock_save_as.png", 
+                       proc{ gui_level_save_as() })
 
-# copy_icon    = Icon.new(CL_Rect(CL_Point(p.inc(48), 2), CL_Size(32, 32)),
-#                     make_sprite("../data/images/icons24/stock_copy.png"), "Some tooltip", button_panel);
-# paste_icon   = Icon.new(CL_Rect(CL_Point(p.inc(32), 2), CL_Size(32, 32)),
-#                     make_sprite("../data/images/icons24/stock_paste.png"), "Some tooltip", button_panel);
-
-# undo_icon = Icon.new(CL_Rect.new(CL_Point.new(p.inc(48), 2), CL_Size(32, 32)),
-#                  make_sprite("../data/images/icons24/stock_undo.png"), "Some tooltip", button_panel);
-# redo_icon = Icon.new(CL_Rect.new(CL_Point.new(p.inc(32), 2), CL_Size(32, 32)),
-#                  make_sprite("../data/images/icons24/stock_redo.png"), "Some tooltip", button_panel);
-
-# undo_icon.set_callback(lambda: $workspace.get_map().undo())
-# redo_icon.set_callback(lambda: $workspace.get_map().redo())
-
-# undo_icon.disable()
-# redo_icon.disable()
+$button_panel.add_seperator()
+$button_panel.add_icon("../data/images/icons24/stock_copy.png", proc{})
+$button_panel.add_icon("../data/images/icons24/stock_paste.png", proc{})
+$button_panel.add_seperator()
+$undo_icon = $button_panel.add_icon("../data/images/icons24/stock_undo.png", proc{$workspace.get_map().undo()})
+$redo_icon = $button_panel.add_icon("../data/images/icons24/stock_redo.png", proc{$workspace.get_map().redo()})
 
 def gui_toggle_grid()
   tilemap = $workspace.get_map().get_metadata().data.get_tilemap()
@@ -231,58 +217,56 @@ end
 
 def set_tilemap_paint_tool()
   $workspace.set_tool($tilemap_paint_tool.to_tool())
-#   $paint.set_down()
-#   $select.set_up()
-#   $zoom.set_up()
-#   $object.set_up()
+  $paint.set_down()
+  $select.set_up()
+  $zoom.set_up()
+  $object.set_up()
 end
 
 def set_tilemap_select_tool()
   $workspace.set_tool($tilemap_select_tool.to_tool())
-#   $paint.set_up()
-#   $select.set_down()
-#   $zoom.set_up()
-#   $object.set_up()
+  $paint.set_up()
+   $select.set_down()
+  $zoom.set_up()
+  $object.set_up()
 end
 
 def set_zoom_tool()
   $workspace.set_tool($zoom_tool.to_tool())
-#   $paint.set_up()
-#   $select.set_up()
-#   $zoom.set_down()
-#   $object.set_up()
+  $paint.set_up()
+  $select.set_up()
+  $zoom.set_down()
+  $object.set_up()
 end
 
 def set_objmap_select_tool()
   $workspace.set_tool($objmap_select_tool.to_tool())
-#   $paint.set_up()
-#   $select.set_up()
-#   $zoom.set_up()
-#   $object.set_down()
+  $paint.set_up()
+  $select.set_up()
+  $zoom.set_up()
+  $object.set_down()
 end
 
-toolbar = Panel.new(CL_Rect.new(CL_Point.new(0, 23+33), CL_Size.new(33, 32*4+2)), $gui.get_component())
+$toolbar = Panel.new(CL_Rect.new(CL_Point.new(0, 23+33), CL_Size.new(33, 32*4+2)), $gui.get_component())
 
-paint = Icon.new(CL_Rect.new(CL_Point.new(2, 32*0+2), CL_Size.new(32, 32)), make_sprite("../data/images/tools/stock-tool-pencil-22.png"), "Some tooltip", toolbar);
-paint.set_callback(method(:set_tilemap_paint_tool))
+$paint = Icon.new(CL_Rect.new(CL_Point.new(2, 32*0+2), CL_Size.new(32, 32)), make_sprite("../data/images/tools/stock-tool-pencil-22.png"), "Some tooltip", toolbar);
+$paint.set_callback(method(:set_tilemap_paint_tool))
 
-select = Icon.new(CL_Rect.new(CL_Point.new(2, 32*1+2), CL_Size.new(32,32)), make_sprite("../data/images/tools/stock-tool-rect-select-22.png"), "Some tooltip", toolbar);
-select.set_callback(method(:set_tilemap_select_tool))
+$select = Icon.new(CL_Rect.new(CL_Point.new(2, 32*1+2), CL_Size.new(32,32)), make_sprite("../data/images/tools/stock-tool-rect-select-22.png"), "Some tooltip", toolbar);
+$select.set_callback(method(:set_tilemap_select_tool))
 
-zoom = Icon.new(CL_Rect.new(CL_Point.new(2, 32*2+2), CL_Size.new(32,32)), make_sprite("../data/images/tools/stock-tool-zoom-22.png"), "Some tooltip", toolbar);
-zoom.set_callback(method(:set_zoom_tool))
+$zoom = Icon.new(CL_Rect.new(CL_Point.new(2, 32*2+2), CL_Size.new(32,32)), make_sprite("../data/images/tools/stock-tool-zoom-22.png"), "Some tooltip", toolbar);
+$zoom.set_callback(method(:set_zoom_tool))
 
-object = Icon.new(CL_Rect.new(CL_Point.new(2, 32*3+2), CL_Size.new(32,32)), make_sprite("../data/images/tools/stock-tool-clone-22.png"), "Some tooltip", toolbar);
-object.set_callback(method(:set_objmap_select_tool))
-
-level = nil
+$object = Icon.new(CL_Rect.new(CL_Point.new(2, 32*3+2), CL_Size.new(32,32)), make_sprite("../data/images/tools/stock-tool-clone-22.png"), "Some tooltip", toolbar);
+$object.set_callback(method(:set_objmap_select_tool))
 
 mysprite = make_sprite("../data/images/icons16/stock_paste-16.png")
 
 def netpanzer_load_level(filename)
-  level = Level(filename)
-  level.activate(workspace)
-  connect(level.editormap.sig_change(), on_map_change)
+  level = Level.new(filename)
+  level.activate($workspace)
+  connect(level.editormap.sig_change(), proc{on_map_change})
   
   if not(has_element(config.recent_files, filename))
     config.recent_files.append(filename)

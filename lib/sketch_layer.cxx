ad5813ddd5172461e0a98c2c13daecc8501483ea
Ingo Ruhnke <grumbel@gmail.com>
2004-12-13 22:08:18 +0000

    - some more interesting brush support

--- lib/sketch_layer.cxx
+++ lib/sketch_layer.cxx
@@ -21,7 +21,9 @@
 #include <ClanLib/gl.h>
 #include <ClanLib/Core/core_iostream.h>
 #include <ClanLib/Display/display.h>
+#include <ClanLib/Display/sprite.h>
 #include <ClanLib/Display/display_window.h>
+#include "flexlay.hxx"
 #include "layer_impl.hxx"
 #include "sketch_layer.hxx"
 
@@ -68,79 +70,174 @@ class SketchLayerImpl : public LayerImpl
 public:
   typedef std::vector<Stroke> Strokes;
   Strokes strokes;
+  CL_Sprite brush;
 
+  SketchLayerImpl() 
+    : brush("brush", &(Flexlay::current()->resources))
+  {    
+  }
+  
   void draw(EditorMapComponent* parent) 
   {
-    CL_OpenGLState state(CL_Display::get_current_window()->get_gc());
-    state.set_active();
-    state.setup_2d();
 
-    glLineWidth(3.0);
-    
+    //glLineWidth(3.0);
+
     for(Strokes::iterator i = strokes.begin(); i != strokes.end(); ++i)
       {
-        if (i->points.size() >= 2)
+        float spacing = 8.0f;
+        brush.set_color(i->color);
+        
+        if (i->points.size() == 1 || i->points.size() == 2)
           {
-            /*
-            glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
-            glBegin(GL_LINE_STRIP);
-            for(Stroke::Points::iterator j = i->points.begin(); j != i->points.end(); ++j)
-              glVertex2f(j->x, j->y);
-              glEnd();*/
-
-            float len = 1.0f;
-            float len2 = 2.0f;
-            glEnable(GL_BLEND);
-            glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-            glBegin(GL_QUADS);
+            brush.draw((int)i->points.front().x, (int)i->points.front().y);
+          }
+        else
+          {
+            float overspace = 0.0f;
             for(unsigned int j = 0; j < i->points.size()-1; ++j)
               {
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
-                glVertex2f(i->points[j].x + i->normals[j].x * len,
-                           i->points[j].y + i->normals[j].y * len);
-                glVertex2f(i->points[j].x - i->normals[j].x * len,
-                           i->points[j].y - i->normals[j].y * len);
-                glVertex2f(i->points[j+1].x - i->normals[j].x * len,
-                           i->points[j+1].y - i->normals[j].y * len);
-                glVertex2f(i->points[j+1].x + i->normals[j].x * len,
-                           i->points[j+1].y + i->normals[j].y * len);
+                CL_Pointf dist = i->points[j+1] - i->points[j];
+                float length = sqrt(dist.x * dist.x + dist.y * dist.y);
+                int n = 1;
 
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
-                glVertex2f(i->points[j].x + i->normals[j].x * len2,
-                           i->points[j].y + i->normals[j].y * len2);
+                while (length + overspace > (spacing * n))
+                  {
+                    float factor = (spacing/length) * n - (overspace/length);
+                    CL_Pointf p(i->points[j].x + dist.x * factor,
+                                i->points[j].y + dist.y * factor);
+                    brush.draw((int)p.x, (int)p.y);
+                
+                    n += 1;
+                  }
 
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
-                glVertex2f(i->points[j+1].x + i->normals[j].x * len2,
-                           i->points[j+1].y + i->normals[j].y * len2);
-
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
-                glVertex2f(i->points[j+1].x + i->normals[j].x * len,
-                           i->points[j+1].y + i->normals[j].y * len);
-
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
-                glVertex2f(i->points[j].x + i->normals[j].x * len,
-                           i->points[j].y + i->normals[j].y * len);
-
-                /////////////
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
-                glVertex2f(i->points[j].x - i->normals[j].x * len,
-                           i->points[j].y - i->normals[j].y * len);
-
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
-                glVertex2f(i->points[j].x - i->normals[j].x * len2,
-                           i->points[j].y - i->normals[j].y * len2);
-
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
-                glVertex2f(i->points[j+1].x - i->normals[j].x * len2,
-                           i->points[j+1].y - i->normals[j].y * len2);
-                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
-                glVertex2f(i->points[j+1].x - i->normals[j].x * len,
-                           i->points[j+1].y - i->normals[j].y * len);
+                // Space that wasn't used in the last iteration
+                overspace = (length + overspace) - (spacing * (n-1));
+                //std::cout << "Overspace: " << overspace << std::endl;
               }
-            glEnd();
           }
       }
-    glLineWidth(1.0);
+
+    {
+      CL_OpenGLState state(CL_Display::get_current_window()->get_gc());
+      state.set_active();
+      state.setup_2d();
+
+      if (0)
+        for(Strokes::iterator i = strokes.begin(); i != strokes.end(); ++i)
+          {
+            if (i->points.size() >= 2)
+              {
+                float len = 1.0f;
+                float len2 = 2.0f;
+            
+                glEnable(GL_BLEND);
+                glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+                glBegin(GL_QUAD_STRIP);
+                for(unsigned int j = 0; j < i->points.size()-2; ++j)
+                  {
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j].x + i->normals[j].x * len,
+                               i->points[j].y + i->normals[j].y * len);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
+                    glVertex2f(i->points[j].x + i->normals[j].x * len2,
+                               i->points[j].y + i->normals[j].y * len2);
+                  }
+                glEnd();
+
+                glBegin(GL_QUAD_STRIP);
+                for(unsigned int j = 0; j < i->points.size()-2; ++j)
+                  {
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
+                    glVertex2f(i->points[j].x - i->normals[j].x * len2,
+                               i->points[j].y - i->normals[j].y * len2);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j].x - i->normals[j].x * len,
+                               i->points[j].y - i->normals[j].y * len);
+                  }
+                glEnd();
+
+                glBegin(GL_QUAD_STRIP);
+                glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                for(unsigned int j = 0; j < i->points.size()-2; ++j)
+                  {
+                    glVertex2f(i->points[j].x + i->normals[j].x * len,
+                               i->points[j].y + i->normals[j].y * len);
+                    glVertex2f(i->points[j].x - i->normals[j].x * len,
+                               i->points[j].y - i->normals[j].y * len);
+                  }
+                glEnd();
+              }
+          }
+
+      if (0)
+        for(Strokes::iterator i = strokes.begin(); i != strokes.end(); ++i)
+          {
+            if (i->points.size() >= 2)
+              {
+                /*
+                  glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                  glBegin(GL_LINE_STRIP);
+                  for(Stroke::Points::iterator j = i->points.begin(); j != i->points.end(); ++j)
+                  glVertex2f(j->x, j->y);
+                  glEnd();*/
+
+                float len = 1.0f;
+                float len2 = 2.0f;
+                glEnable(GL_BLEND);
+                glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+                glBegin(GL_QUADS);
+                for(unsigned int j = 0; j < i->points.size()-1; ++j)
+                  {
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j].x + i->normals[j].x * len,
+                               i->points[j].y + i->normals[j].y * len);
+                    glVertex2f(i->points[j].x - i->normals[j].x * len,
+                               i->points[j].y - i->normals[j].y * len);
+                    glVertex2f(i->points[j+1].x - i->normals[j].x * len,
+                               i->points[j+1].y - i->normals[j].y * len);
+                    glVertex2f(i->points[j+1].x + i->normals[j].x * len,
+                               i->points[j+1].y + i->normals[j].y * len);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
+                    glVertex2f(i->points[j].x + i->normals[j].x * len2,
+                               i->points[j].y + i->normals[j].y * len2);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
+                    glVertex2f(i->points[j+1].x + i->normals[j].x * len2,
+                               i->points[j+1].y + i->normals[j].y * len2);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j+1].x + i->normals[j].x * len,
+                               i->points[j+1].y + i->normals[j].y * len);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j].x + i->normals[j].x * len,
+                               i->points[j].y + i->normals[j].y * len);
+
+                    /////////////
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j].x - i->normals[j].x * len,
+                               i->points[j].y - i->normals[j].y * len);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
+                    glVertex2f(i->points[j].x - i->normals[j].x * len2,
+                               i->points[j].y - i->normals[j].y * len2);
+
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), 0);
+                    glVertex2f(i->points[j+1].x - i->normals[j].x * len2,
+                               i->points[j+1].y - i->normals[j].y * len2);
+                    glColor4ub(i->color.get_red(), i->color.get_green(), i->color.get_blue(), i->color.get_alpha());
+                    glVertex2f(i->points[j+1].x - i->normals[j].x * len,
+                               i->points[j+1].y - i->normals[j].y * len);
+                  }
+                glEnd();
+              }
+          }
+      //glLineWidth(1.0);
+    }
   }
 
   bool has_bounding_rect() const { 

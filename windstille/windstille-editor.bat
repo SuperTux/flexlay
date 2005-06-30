f2f9a3e4a6ca6cbf61f26f5943add0b6ec26ca24
James Gregory <jamesgregory@users.sourceforge.net>
2005-06-30 12:36:34 +0000

    wicked MSDOS batch script

---
+++ windstille/windstille-editor.bat
@@ -0,0 +1,7 @@
+if %already_run%.==yes. goto runit
+set already_run=yes
+set RUBYLIB=%RUBYLIB%;..\lib;..\ruby
+
+:runit
+REM edit this to point to wherever your ruby.exe is located (or delete the path altogether if ruby.exe is already in your PATH)
+call c:\libraries\ruby\ruby.exe windstille.rb

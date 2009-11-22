f09ecf451a9e15f2ba882510e6ae19a95f0bcb98
Ingo Ruhnke <grumbel@gmail.com>
2009-11-22 19:34:12 +0000

    More Copyright header cleanup

--- flexlay/lib/stroke_drawer_impl.hpp
+++ flexlay/lib/stroke_drawer_impl.hpp
@@ -1,4 +1,4 @@
-//  Pingus - A free Lemmings clone
+//  Flexlay - A Generic 2D Game Editoryy
 //  Copyright (C) 2002 Ingo Ruhnke <grumbel@gmx.de>
 //
 //  This program is free software: you can redistribute it and/or modify

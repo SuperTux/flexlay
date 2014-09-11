9943a2ecb5cfe5d7339ec097f30a85a84a24839b
Ingo Ruhnke <grumbel@gmail.com>
2014-09-11 07:49:52 +0200

    Implemented Commands in Python

---
+++ flexlay/object_add_command.py
@@ -0,0 +1,42 @@
+# Flexlay - A Generic 2D Game Editor
+# Copyright (C) 2014 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from flexlay import Command
+
+
+class ObjectAddCommand(Command):
+
+    def __init__(self, objmap):
+        self.objmap = objmap
+        self.objs = []
+
+    def add_object(self, obj):
+        self.objs.append(obj)
+
+    def execute(self):
+        for obj in self.objs:
+            self.objmap.add_object(obj)
+
+    def undo(self):
+        for obj in self.objs:
+            self.objmap.delete_object(obj)
+
+    def redo(self):
+        self.execute()
+
+
+# EOF #
---
+++ flexlay/paint_command.py
@@ -0,0 +1,79 @@
+# Flexlay - A Generic 2D Game Editor
+# Copyright (C) 2014 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from flexlay.math import Rect
+from flexlay import Command, TileBrush, TilemapLayer
+
+
+class PaintCommand(Command):
+
+    def __init__(self, tilemap_layer, brush):
+        self.points = []
+
+        self.tilemap = tilemap_layer
+        self.brush = brush
+
+        # Copy of the field used to generate undo informations */
+        self.undo_field = self.tilemap.get_field().copy()
+
+        self.pos = None
+        self.redo_brush = None
+        self.undo_brush = None
+
+    def add_point(self, pos):
+        # FIXME: undo_field is unneeded, should just record the overwritten color
+        self.points.push_back(pos)
+        self.tilemap.draw_tile(self.brush, pos)
+
+    def execute(self):
+        assert self.points != []
+
+        # Calc bounding rect
+        rect = Rect(self.points.front().x,
+                    self.points.front().y,
+                    self.points.front().x + self.brush.get_width(),
+                    self.points.front().y + self.brush.get_height())
+
+        for point in self.points:
+            rect.left = min(rect.left, point.x)
+            rect.top = min(rect.top, point.y)
+            rect.right = max(rect.right, point.x + self.brush.get_width())
+            rect.bottom = max(rect.bottom, point.y + self.brush.get_height())
+
+        self.pos.x = rect.left
+        self.pos.y = rect.top
+
+        self.redo_brush = TileBrush(self.tilemap.get_field().copy(), rect.get_width(), rect.get_height(),
+                                    -self.pos.x, -self.pos.y)
+
+        # FIXME: undo_field is unneeded, should just record the overwritten color
+        self.undo_brush = TileBrush(self.undo_field, rect.get_width(), rect.get_height(),
+                                    -self.pos.x, -self.pos.y)
+
+        self.redo_brush.set_opaque()
+        self.undo_brush.set_opaque()
+
+        self.undo_field.clear()
+
+    def redo(self):
+        TilemapLayer.draw_tiles(self.tilemap.get_field(), self.redo_brush, self.pos)
+
+    def undo(self):
+        TilemapLayer.draw_tiles(self.tilemap.get_field(), self.undo_brush, self.pos)
+
+
+# EOF #

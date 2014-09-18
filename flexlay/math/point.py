99a804833bcc3c08323b4cefb6c0c2caf6a2c299
Ingo Ruhnke <grumbel@gmail.com>
2014-09-18 17:05:50 +0200

    Added multiplication support to Point

--- flexlay/math/point.py
+++ flexlay/math/point.py
@@ -46,6 +46,14 @@ class Point:
         return Point(self.x - rhs.x,
                      self.y - rhs.y)
 
+    def __mul__(self, rhs):
+        return Point(self.x * rhs,
+                     self.y * rhs)
+
+    def __rmul__(self, rhs):
+        return Point(self.x * rhs,
+                     self.y * rhs)
+
     def __eq__(self, rhs):
         return self.x == rhs.x and self.y == rhs.y
 

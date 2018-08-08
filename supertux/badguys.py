0306118e5579bb653ad5bab1918f8c212fc2e575
Ingo Ruhnke <grumbel@gmail.com>
2018-08-08 21:26:15 +0200

    Moved badguy sprites from .csv to badguys.py

---
+++ supertux/badguys.py
@@ -0,0 +1,77 @@
+# Flexlay - A Generic 2D Game Editor
+# Copyright (C) 2014 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+badguy_sprites = [
+    ["pneumatic-platform", "images/engine/editor/pneumaticplatform.png"],
+    ["bicycle-platform", "images/engine/editor/bicycleplatform.png"],
+    ["flying-platform", "images/objects/flying_platform/flying_platform.sprite"],
+    ["hurting_platform", "images/objects/sawblade/sawblade.sprite"],
+    ["angrystone", "images/creatures/angrystone/angrystone.sprite"],
+    ["bouncingsnowball", "images/creatures/bouncing_snowball/left-0.png"],
+    ["captainsnowball", "images/creatures/snowball/cpt-left-0.png"],
+    ["climbable", "images/engine/editor/climbable.png"],
+    ["crystallo", "images/creatures/crystallo/crystallo.sprite"],
+    ["fish", "images/creatures/fish/left-0.png"],
+    ["flame", "images/creatures/flame/flame-0.png"],
+    ["flyingsnowball", "images/creatures/flying_snowball/left-0.png"],
+    ["ghosttree", "images/creatures/flame/ghostflame.sprite"],
+    ["goldbomb", "images/creatures/gold_bomb/gold_bomb.sprite"],
+    ["haywire", "images/creatures/haywire/haywire.sprite"],
+    ["icecrusher", "images/creatures/icecrusher/icecrusher.sprite"],
+    ["iceflame", "images/creatures/flame/iceflame.sprite"],
+    ["igel", "images/creatures/igel/igel.sprite"],
+    ["ispy", "images/objects/ispy/ispy.sprite"],
+    ["jumpy", "images/creatures/snowjumpy/left-middle.png"],
+    ["kugelblitz", "images/creatures/kugelblitz/flying-0.png"],
+    ["lantern", "images/objects/lantern/lantern.sprite"],
+    ["livefire", "images/creatures/livefire/livefire.sprite"],
+    ["livefire_asleep", "images/creatures/livefire/livefire.sprite"],
+    ["livefire_dormant", "images/creatures/livefire/livefire.sprite"],
+    ["magicblock", "images/objects/magicblock/magicblock.sprite"],
+    ["mole", "images/creatures/mole/mole.sprite"],
+    ["mrbomb", "images/creatures/mr_bomb/left.png"],
+    ["mriceblock", "images/creatures/mr_iceblock/left-0.png"],
+    ["mrtree", "images/creatures/mr_tree/walk-left-1.png"],
+    ["owl", "images/creatures/owl/owl.sprite"],
+    ["particles-ghosts", "images/engine/editor/ghostparticles.png"],
+    ["poisonivy", "images/creatures/poison_ivy/left-0.png"],
+    ["rustytrampoline", "images/objects/rusty-trampoline/rusty-trampoline.sprite"],
+    ["short_fuse", "images/creatures/short_fuse/short_fuse.sprite"],
+    ["skullyhop", "images/creatures/skullyhop/skullyhop.sprite"],
+    ["smartball", "images/creatures/snowball/left-1.png"],
+    ["smartblock", "images/creatures/mr_iceblock/smart_block/smart_block.sprite"],
+    ["snail", "images/creatures/snail/snail.sprite"],
+    ["snowball", "images/creatures/snowball/sport-left-1.png"],
+    ["snowman", "images/creatures/snowman/snowman.sprite"],
+    ["spidermite", "images/creatures/spidermite/spidermite.sprite"],
+    ["spiky", "images/creatures/spiky/left-0.png"],
+    ["spotlight", "images/objects/spotlight/spotlight_center.sprite"],
+    ["sspiky", "images/creatures/spiky/sleeping-left.png"],
+    ["stalactite", "images/creatures/stalactite/falling.png"],
+    ["stalactite_yeti", "images/engine/editor/stalactite_yeti.png"],
+    ["stumpy", "images/creatures/mr_tree/stumpy.sprite"],
+    ["thunderstorm", "images/engine/editor/thunderstorm.png"],
+    ["toad", "images/creatures/toad/toad.sprite"],
+    ["totem", "images/creatures/totem/totem.sprite"],
+    ["walkingleaf", "images/creatures/walkingleaf/walkingleaf.sprite"],
+    ["walkingtree", "images/creatures/walkingleaf/walkingleaf.sprite"],
+    ["yeti", "images/creatures/yeti/yeti.png"],
+    ["yeti_stalactite", "images/engine/editor/stalactite_yeti.png"],
+    ["zeekling", "images/creatures/zeekling/left-0.png"],
+]
+
+# EOF #

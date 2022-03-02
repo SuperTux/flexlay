54749437bd326bc85ede1fc18478290df2b17a42
Ingo Ruhnke <grumbel@gmail.com>
2022-03-02 13:14:07 +0100

    Move text_editor/ into supertux/ directory

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='flexlay',
       entry_points={
           'console_scripts': [
               'supertux-editor = supertux.supertux:main',
-              'supertux-texteditor = text_editor.cmd_supertux_textedit:main',
+              'supertux-texteditor = supertux.text_editor.cmd_supertux_textedit:main',
           ]
       },
       packages=find_packages()
--- supertux/text_editor/cmd_supertux_textedit.py
+++ supertux/text_editor/cmd_supertux_textedit.py
@@ -22,8 +22,8 @@ from PyQt5.QtWidgets import (QMainWindow, QApplication, QVBoxLayout,
                          QWidget)
 from PyQt5.QtCore import Qt
 
-from text_editor.supertux_text_edit import SupertuxTextEdit
-from text_editor.highlighters.lisp_highlighter import SuperTuxLispHighlighter
+from supertux.text_editor.supertux_text_edit import SupertuxTextEdit
+from supertux.text_editor.highlighters.lisp_highlighter import SuperTuxLispHighlighter
 
 
 class SupertuxTextEditMainWindow(QMainWindow):
